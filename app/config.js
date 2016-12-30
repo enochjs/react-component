58c58663cf08bef576e779e6ddb640053328f323
王永东 <enochjs@16.com>
2016-12-30 17:32:49 +0800

    config style modify

--- app/config.js
+++ app/config.js
@@ -1,12 +1,12 @@
 const config = {
 	menuList: [
-		{
-		  "name":"Table",
-      "url":"",
-      "children":[
-          { "name":"ExpandTable", "route":"expandTable" },
-      ]
-    }
+        {
+            "name":"Table",
+            "url":"",
+            "children":[
+                { "name":"ExpandTable", "route":"expandTable" },
+            ]
+        }
 	]
 }
 
